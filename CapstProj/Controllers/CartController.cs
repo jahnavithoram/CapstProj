b29eecb95f203344ed0faf1d642d4a011707945b
Jahnavi Thoram <jahnavithoram999@gmail.com>
2021-05-20 03:52:18 +0530

    "userConfirmOrder"

--- CapstProj/Controllers/CartController.cs
+++ CapstProj/Controllers/CartController.cs
@@ -27,7 +27,10 @@ namespace CapstProj.Controllers
         {
             return View(await _context.CartModel.ToListAsync());
         }
-
+        public async Task<IActionResult> pay()
+        {
+            return View();
+        }
         // GET: Cart/Details/5
         public async Task<IActionResult> Details(string id)
         {
@@ -159,5 +162,6 @@ namespace CapstProj.Controllers
             }
             return RedirectToAction("Index1", "Product"); ;
         }
+      
     }
 }
